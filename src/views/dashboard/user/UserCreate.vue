3ff0953fd766b0232484bc5cf0d401d1daad040a
shahinyanm <shahinyanm@gmail.com>
2020-12-24 19:06:09 +0400

    add user Create

--- src/views/dashboard/user/UserCreate.vue
+++ src/views/dashboard/user/UserCreate.vue
@@ -11,23 +11,23 @@
                 <div class="intro-y box p-5">
                     <div>
                         <label>Name</label>
-                        <input type="text" class="input w-full border mt-2" placeholder="Input text">
+                        <input type="text" class="input w-full border mt-2" :class="{'border-red-800':nameError}" placeholder="Input text" v-model="form.userName">
                     </div>
                     <div class="mt-3">
                         <label>Email</label>
-                        <input type="text" class="input w-full border mt-2" placeholder="Enter user email">
+                        <input type="text" class='input w-full border mt-2' :class="{'border-red-800':emailError}"  placeholder="Enter user email" v-model="form.email">
                     </div>
                     <div class="mt-3">
                         <label>Position</label>
-                        <input type="text" class="input w-full border mt-2" placeholder="Enter user position">
+                        <input type="text" class="input w-full border mt-2" :class="{'border-red-800':positionError}" placeholder="Enter user position" v-model="form.position">
                     </div>
                     <div class="mt-3">
                         <label>Progress</label>
-                        <input type="number" class="input w-full border mt-2" placeholder="Enter user progress">
+                        <input type="number" class="input w-full border mt-2" :class="{'border-red-800':progressError}" placeholder="Enter user progress" v-model="form.progress">
                     </div>
                     <div class="text-right mt-5">
-                        <button type="button" class="button w-24 border dark:border-dark-5 text-gray-700 dark:text-gray-300 mr-1">Cancel</button>
-                        <button type="button" class="button w-24 bg-theme-1 text-white">Save</button>
+                        <button type="button" class="button w-24 border dark:border-dark-5 text-gray-700 dark:text-gray-300 mr-1" @click="cancel">Cancel</button>
+                        <button type="button" class="button w-24 bg-theme-1 text-white" @click="save">Save</button>
                     </div>
                 </div>
                 <!-- END: Form Layout -->
@@ -39,6 +39,37 @@
 <script>
   export default {
     name: 'UserCreate',
+    data(){
+      return {
+        form:{
+          userName:'',
+          email:'',
+          position:'',
+          progress:0
+        },
+        nameError: false,
+        emailError:false,
+        positionError:false,
+        progressError:false,
+
+      }
+    },
+    methods:{
+      cancel(){
+        this.form={
+          userName:'',
+            email:'',
+            position:'',
+            progress:0
+        }
+        this.$router.push({ name: 'Login' })
+      },
+      save(){
+        let users = JSON.parse(window.localStorage.getItem('users'))
+        localStorage.setItem('user', JSON.stringify(this.form))
+
+      },
+    }
   }
 </script>
 
